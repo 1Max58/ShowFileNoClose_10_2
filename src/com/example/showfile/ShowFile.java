34ff900bea4ac0103fec0ea365b73c143d73ef31
1Max58 <mkorsun1@stu.vistula.edu.pl>
2023-12-21 11:14:25 +0200

    try-with-resources functionality used to omit close() method call

--- src/com/example/showfile/ShowFile.java
+++ src/com/example/showfile/ShowFile.java
@@ -1,36 +1,20 @@
 package com.example.showfile;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class ShowFile {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        FileInputStream fis;
         int bajt;
 
-        try {
-            fis = new FileInputStream("test123.txt");
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found");
-            return;
-        }
-
-        try {
+        try (FileInputStream fis = new FileInputStream("test123.txt")) {
             do {
                 bajt = fis.read();
                 if (bajt != -1) System.out.println((char) bajt);
             } while (bajt != -1);
         } catch (IOException e) {
             System.out.println("Error reading file");
-        } finally {
-            try {
-                fis.close();
-            } catch (IOException e) {
-                System.out.println("Error closing file.");
-            }
         }
     }
 }
